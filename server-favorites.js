80548b479371c308ca22fec0b78d8f90c46abe9c
kelichau <tchau2@outlook.com>
2017-12-19 14:31:39 -0500

    Adds working code to access server with index.html

--- server-favorites.js
+++ server-favorites.js
@@ -2,3 +2,23 @@
 var http = require("http"); // http module
 var fs = require("fs"); // fs module
 
+// Setting port to 8080
+var port = 8080;
+
+// Create server
+var server = http.createServer(handleRequest);
+
+// Create a function for handling requests and responses coming into server
+function handleRequest(require, response){
+	// Use fs package to read html files
+	fs.readFile(__dirname + "/index.html", function(error, data){
+		// Respond to client with HTML page by specifically telling the browser that we're delivering an HTML file
+		response.writeHead(200, {"Content-Type": "text/html"});
+		response.end(data);
+	});
+}
+
+// Start server
+server.listen(port, function(){
+	console.log("Server is listening on port: " + port);
+});
